33b4e515a00f6c91f64c300b629558217bf729ec
Lihang He <kxyjm.911@gmail.com>
2022-12-19 16:03:00 -0800

    Update application.py

--- application.py
+++ application.py
@@ -7,9 +7,8 @@ application = flask.Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    google_url = "https://www.bing.com"
-    for i in range(1, 10):
-        requests.get(google_url)
+    google_url = "https://www.google.com"
+    requests.get(google_url)
        
 
     return flask.jsonify({
